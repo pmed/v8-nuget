8289ed614fc48ff5cd72ae766bc3427d3e63193d
Pavel Medvedev <pmedvedev@gmail.com>
2017-05-19 17:20:32 +0300

    Add a batch file to build on appveyor server

---
+++ appveyor-build.bat
@@ -0,0 +1,8 @@
+if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
+  call vswhere_usability_wrapper.cmd
+  call "%VCINSTALLDIR%\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
+  set VisualStudioVersion='15.0'
+)
+
+python build.py
+nuget push *.nupkg %NUGET_API_KEY% -Source https://nuget.org/
