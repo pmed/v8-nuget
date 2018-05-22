ab1dd738f72e14993fa802a1359c3621fcd0cf61
Pavel Medvedev <pmedvedev@gmail.com>
2018-05-22 15:37:30 +0200

    call `vcvarsall.bat` for Appveyor "Visual Studio 2015" build image
    in order to setup proper C++ toolset environment

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -3,6 +3,9 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
   call "%VCINSTALLDIR%\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
   set VisualStudioVersion=15.0
 )
+if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
+  call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
+)
 
 python build.py
 nuget push *.nupkg %NUGET_API_KEY% -Source https://nuget.org/
