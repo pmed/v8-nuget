1bc79f0891ec5eb2f9bbe090a853f06ba3350eba
Pavel Medvedev <pmedvedev@gmail.com>
2025-01-02 11:06:39 +0100

    use `depot_tools/gclient sync` for V8 fetching
    - sync with `--no-history`, `--shallow` flags to get only the specified V8 version ref and its dependencies
    
    - use `gn.exe`, `ninja.exe` from the fetched dependencies
    
    - remove old Visual Studio versions and XP support
    
    - remove `--use-clang` flag, since it's now default build option because V8 component build is broken for Visual C++

--- build.py
+++ build.py
@@ -10,26 +10,12 @@ import sys
 import subprocess
 import shutil
 import urllib.request
-import tarfile
+import io
+import zipfile
 
 BIN_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bin')
 GN_OPTIONS = {
-	'treat_warnings_as_errors' : False,
-	'fatal_linker_warnings' : False,
-	'use_jumbo_build' : True, # removed in V8 version 8.1
-	#'symbol_level' : 1,
-	'v8_enable_fast_mksnapshot' : False,
-	'v8_enable_fast_torque' : False,
-	'v8_enable_verify_heap' : False, # to fix VC++ Linker error in Debug configuratons
-	#'v8_optimized_debug' : False,
-	#'v8_use_snapshot' : True,
 	'v8_use_external_startup_data' : False,
-	#'v8_enable_handle_zapping' : True,
-	#'v8_check_header_includes' : True,
-	#'v8_win64_unwinding_info' : False,
-	#'dcheck_always_on' : True,
-	#'is_clang': USE_CLANG,
-	'use_custom_libcxx' : False,
 }
 
 def parse_to_dict(action, parser, namespace, values, option_string):
@@ -49,25 +35,6 @@ def parse_to_dict(action, parser, namespace, values, option_string):
 	setattr(namespace, action.dest, dict)
 
 arg_parser = argparse.ArgumentParser(description='Build V8 from sources', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-arg_parser.add_argument('--no-fetch',
-	dest='NOFETCH',
-	action='store_true',
-	default=False,
-	help='Do not fetch sources')
-arg_parser.add_argument('--no-git',
-	dest='NOGIT',
-	action='store_true',
-	default=False,
-	help='Download tarball instead of Git fetch')
-arg_parser.add_argument('--read-version',
-	dest='READ_VERSION',
-	action='store_true',
-	default=False,
-	help='Read actual V8 version from v8-version.h , do no build')
-arg_parser.add_argument('--url',
-	dest='V8_URL',
-	default='https://chromium.googlesource.com/v8/v8.git',
-	help='Source url')
 arg_parser.add_argument('--version',
 	dest='V8_VERSION',
 	default='lkgr',
@@ -90,24 +57,6 @@ arg_parser.add_argument('--libs',
 	choices=['shared', 'monolith'],
 	default=['shared', 'monolith'],
 	help='Target libraries')
-arg_parser.add_argument('--xp',
-	dest='XP_TOOLSET',
-	action='store_true',
-	default=False,
-	help='Build for Windows XP toolset')
-arg_parser.add_argument('--use-clang',
-	dest='USE_CLANG',
-	action='store_true',
-	default=False,
-	help='Compile with clang')
-arg_parser.add_argument('--gn',
-	dest='GN',
-	default=os.path.join(BIN_DIR, 'gn.exe'),
-	help='Path to gn executable')
-arg_parser.add_argument('--ninja',
-	dest='NINJA',
-	default=os.path.join(BIN_DIR, 'ninja.exe'),
-	help='Path to ninja executable')
 arg_parser.add_argument('--gn-option',
 	dest='GN_OPTIONS',
 	nargs="+", metavar="KEY=VAL",
@@ -125,113 +74,11 @@ if args.V8_VERSION.count('.') < 2 and all(x.isdigit() for x in args.V8_VERSION.s
 
 print('Parsed args: ', args)
 
-
-def is_sha1(ref):
-	if len(ref) == 40:
-		try:
-			sha1 = int(ref, 16)
-			return True
-		except:
-			pass
-	return False
-
-
-def git_fetch(url, target):
-	# input url like: https://chromium.googlesource.com/v8/v8.git@ref
-	if isinstance(url, dict):
-		url = url['url']
-	parts = url.split('.git@')
-	if len(parts) > 1:
-		url = parts[0] + '.git'
-		ref = parts[1]
-	else:
-		ref = 'HEAD'
-
-	if args.NOGIT:
-		# tarball urls:
-		# https://chromium.googlesource.com/chromium/src/build.git/+archive/5c9250c64c70a2f861a435158b57a6d43cd2e7b7.tar.gz
-		# https://chromium.googlesource.com/v8/v8.git/+archive/refs/heads/10.6-lkgr.tar.gz
-		url += '/+archive/' + ('refs/heads/' if not is_sha1(ref) else '') + ref + '.tar.gz'
-	
-		print(f'Download {url} into {target}')
-		with urllib.request.urlopen(url) as stream:
-			with tarfile.open(fileobj=stream, mode="r|gz") as tar:
-				tar.extractall(path=target)
-	else:
-		print(f'Git fetch {url}@{ref} into {target}')
-
-		if not os.path.isdir(os.path.join(target, '.git')):
-			subprocess.check_call(['git', 'init', target])
-		fetch_args = ['git', 'fetch', '--depth=1', '--update-shallow', '--update-head-ok', '--verbose', url, ref]
-		if subprocess.call(fetch_args, cwd=target) != 0:
-			print(f'RETRY: {target}')
-			shutil.rmtree(target, ignore_errors=True)
-			subprocess.check_call(['git', 'init', target])
-			subprocess.check_call(fetch_args, cwd=target)
-		subprocess.check_call(['git', 'checkout', '-f', '-B', 'Branch_'+ref, 'FETCH_HEAD'], cwd=target)
-
-
-def rmtree(dir):
-	if os.path.isdir(dir):
-		shutil.rmtree(dir)
-
-def copytree(src_dir, dest_dir):
-	if not os.path.isdir(dest_dir):
-		os.makedirs(dest_dir)
-	for path in glob.iglob(src_dir):
-		shutil.copy(path, dest_dir)
-
-
-# __main__
-
-## Fetch V8 sources
-if args.NOFETCH and os.path.exists('v8'):
-	print('Skip fetching, v8 already exists')
-else:
-	git_fetch(args.V8_URL+'@'+args.V8_VERSION, 'v8')
-
-	## Fetch only required V8 source dependencies
-	required_deps = [
-		'v8/build',
-		'v8/third_party/icu',
-		'v8/base/trace_event/common',
-		'v8/third_party/jinja2',
-		'v8/third_party/markupsafe',
-		'v8/third_party/googletest/src',
-		'v8/third_party/zlib',
-		'v8/third_party/abseil-cpp',
-	]
-
-	if args.USE_CLANG:
-		required_deps.append('v8/tools/clang')
-
-	Var = lambda name: vars[name]
-	Str = lambda str: str
-	deps = open('v8/DEPS').read()
-	exec(deps)
-
-	for name, url in deps.items():
-		if not name.startswith('v8'):
-			name = 'v8/' + name
-		if name in required_deps:
-			git_fetch(url, name)
-
-### Get v8 version from defines in v8-version.h
-v8_version_h = open('v8/include/v8-version.h').read()
-version = '.'.join(map(lambda name: re.search(r'^#define\s+'+name+r'\s+(\d+)$', v8_version_h, re.M).group(1), \
-	['V8_MAJOR_VERSION', 'V8_MINOR_VERSION', 'V8_BUILD_NUMBER', 'V8_PATCH_LEVEL']))
-print(f'V8 {version}')
-if args.READ_VERSION:
-	sys.exit(0)
-
 vs_versions = {
-	'12.0': { 'version': '2013', 'toolset': 'v120' },
-	'14.0': { 'version': '2015', 'toolset': 'v140' },
-	'15.0': { 'version': '2017', 'toolset': 'v141' },
 	'16.0': { 'version': '2019', 'toolset': 'v142' },
 	'17.0': { 'version': '2022', 'toolset': 'v143' },
 }
-vs_version = vs_versions[os.environ.get('VisualStudioVersion', '14.0')]
+vs_version = vs_versions[os.environ.get('VisualStudioVersion', '17.0')]
 toolset = vs_version['toolset']
 vs_version = vs_version['version']
 
@@ -242,43 +89,33 @@ if vc_tools_install_dir:
 else:
 	vs_install_dir = os.path.abspath(os.path.join(os.environ['VCINSTALLDIR'], os.pardir))
 
-vc_tools_version = os.environ.get('VCToolsVersion')
-if vc_tools_version:
-	vs_version = vc_tools_version
-	toolset = 'v' + vs_version.replace('.', '')[:3]
-
-
-env = os.environ.copy()
-env['DEPOT_TOOLS_WIN_TOOLCHAIN'] = '0'
-
-if args.XP_TOOLSET:
-	if toolset.startswith('v142'):
-		raise RuntimeError("XP toolset is not supported")
-	env['INCLUDE'] = r'%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;' + env.get('INCLUDE', '')
-	env['PATH'] = r'%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;' + env.get('PATH', '')
-	env['LIB'] = r'%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;' + env.get('LIB', '')
-	toolset += '_xp'
-
-subprocess.check_call([sys.executable, 'vs_toolchain.py', 'update'], cwd='v8/build', env=env)
-if args.USE_CLANG:
-	subprocess.check_call([sys.executable, 'update.py'], cwd='v8/tools/clang/scripts', env=env)
-
 print(f'Visual Studio {vs_version} in {vs_install_dir}')
 print(f'C++ Toolset {toolset}')
 
-# Copy GN to the V8 buildtools in order to work v8gen script
-if not os.path.exists('v8/buildtools/win'):
-    os.makedirs('v8/buildtools/win')
-shutil.copy(args.GN, 'v8/buildtools/win')
 
-# Generate LASTCHANGE file
-# similiar to `lastchange` hook from DEPS
-if os.path.isfile('v8/build/util/lastchange.py'):
-	subprocess.check_call([sys.executable, 'lastchange.py', '-o', 'LASTCHANGE'], cwd='v8/build/util', env=env)
+# Download depot_tools
+if not os.path.isdir('depot_tools'):
+	print('Downloading depot_tools')
+	with urllib.request.urlopen('https://storage.googleapis.com/chrome-infra/depot_tools.zip') as resp:
+		with zipfile.ZipFile(io.BytesIO(resp.read())) as zip:
+			zip.extractall('depot_tools')
 
-if not os.path.isfile('v8/build/config/gclient_args.gni'):
-	with open('v8/build/config/gclient_args.gni', 'a') as f:
-		f.write('declare_args() { checkout_google_benchmark = false }\n')
+env = os.environ.copy()
+#env['DEPOT_TOOLS_UPDATE'] = '0'
+env['DEPOT_TOOLS_WIN_TOOLCHAIN'] = '0'
+
+
+# Fetch V8 and dependencies
+print('Fetching v8')
+subprocess.check_call([os.path.join('depot_tools', 'gclient'), 'sync', '--no-history', '--shallow', '--gclientfile=v8.gclient', '--revision=' + args.V8_VERSION], env=env, shell=True)
+
+
+### Get actual v8 version from defines in v8-version.h
+v8_version_h = open('v8/include/v8-version.h').read()
+version = '.'.join(map(lambda name: re.search(r'^#define\s+'+name+r'\s+(\d+)$', v8_version_h, re.M).group(1), \
+	['V8_MAJOR_VERSION', 'V8_MINOR_VERSION', 'V8_BUILD_NUMBER', 'V8_PATCH_LEVEL']))
+print(f'V8 {version}')
+
 
 def cpp_defines_from_v8_json_build_config(filename):
 	json_file = open(filename)
@@ -312,8 +149,8 @@ def build(target, options, env, out_dir):
 	for k, v in options.items():
 		q = '"' if isinstance(v, str) else ''
 		gn_args.append(k + '=' + q + str(v) + q)
-	subprocess.check_call([args.GN, 'gen', '--ninja-executable=' + args.NINJA, out_dir, '--args=' + ' '.join(gn_args).lower()], cwd='v8', env=env)
-	subprocess.check_call([args.NINJA, '-C', out_dir, target], cwd='v8', env=env)
+	subprocess.check_call([os.path.join('buildtools', 'win', 'gn.exe'), 'gen', out_dir, '--args=' + ' '.join(gn_args).lower()], cwd='v8', env=env, shell=True)
+	subprocess.check_call([os.path.join('third_party', 'ninja', 'ninja.exe'), '-C', out_dir, target], cwd='v8', env=env, shell=True)
 
 
 PACKAGES = {
@@ -331,9 +168,8 @@ for arch in args.PLATFORMS:
 			### Generate build.ninja files in out.gn/V8_VERSION/toolset/arch/conf/lib directory
 			out_dir = os.path.join('out.gn', args.V8_VERSION, toolset, arch, conf, lib)
 			options = args.GN_OPTIONS
-			options['is_debug'] = options['is_full_debug'] = options['enable_iterator_debugging'] = (conf == 'Debug')
+			options['is_debug'] = (conf == 'Debug')
 			options['target_cpu'] = arch
-			options['is_clang'] = args.USE_CLANG
 			options['is_component_build'] = not build_monolith
 			options['v8_monolithic'] = build_monolith
 			target = 'v8'
